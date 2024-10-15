cd91a80e31eef094380c9703c34baf5afdc223c9
Charina González <cesarinagonzalez003@gmail.com>
2024-10-15 11:43:12 -0400

    feat: create a new class 'Employee' that inherits from the class 'Person' and overrides the show details method

--- clases_y_objetos.dart
+++ clases_y_objetos.dart
@@ -3,16 +3,27 @@ class Persona {
     int edad;
 
     Persona({required this.nombre, required this.edad});
+
+    void mostrarDetalles() {
+        print('Nombre: $nombre');
+        print('Edad: $edad'); }
 }
 
-void mostrarDetalles() {
-    print('Nombre: $nombre');
-    print('Edad: $edad'); }
+class Empleado extends Persona {
+    String cargo;
+
+    Empleado({required String nombre, required int edad, required this.cargo})
+        : super(nombre: nombre, edad: edad);
+
+    @override
+    void mostrarDetalles() {
+        super.mostrarDetalles();
+        print('Cargo: $cargo'); }
 }
 
 void main() {
-    Persona persona = Persona(nombre: 'Camila', edad: 23);
+    Empleado empleado = Empleado(nombre: 'Camila', edad: 23, cargo: 'DBA');
 
-    persona.mostrarDetalles();
+    empleado.mostrarDetalles();
 }
 
