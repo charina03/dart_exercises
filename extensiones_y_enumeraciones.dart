272a399a761017c19cae5cc4714e6af68b04c829
Charina González <cesarinagonzalez003@gmail.com>
2024-10-15 12:34:00 -0400

    feat: defines an extension to check if a number entered by the user is an even number

--- extensiones_y_enumeraciones.dart
+++ extensiones_y_enumeraciones.dart
@@ -1,4 +1,4 @@
-enum DiaSemana {
+/*enum DiaSemana {
     lunes,
     martes,
     miercoles,
@@ -11,4 +11,19 @@ void main() {
     for (var dia in DiaSemana.values) {
         print(dia);
     }
+}*/
+
+
+import 'dart:io';
+void main() {
+    print(4.isEven); 
+    print(3.isEven); 
+
+    print('Ingrese un número: ');
+    int numero = int.parse(stdin.readLineSync()!);
+
+    if (numero.isEven) {
+        print('El número $numero es par');
+    } else {
+        print('El número $numero es impar'); }
 }
